c928e48903c4dcc4baec379b51244ba85df802e5
Flavie Bourcet <flavie.bourcet@gmail.com>
2022-09-22 14:04:50 +0200

    ajout de commentaires

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,7 +6,10 @@ const limiter = require('../middleware/rateLimit');
 
 const userCtrl = require('../controllers/user');
 
+//création d'un compte avec verification de l'email ainsi que d'un mdp sécurisé
 router.post('/signup', email, password, userCtrl.signup);
+
+//connexion d'un utilisateur avec limitation des tentatives de connexion
 router.post('/login', limiter, userCtrl.login);
 
 module.exports = router;
